ba977dff59e1a65869f33dc64d2e5ff92a919096
Benjamin Doherty <bjd@bangpound.org>
2016-04-30 15:53:53 -0500

    change to composer plugin type.

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PatternLab\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new UnifiedAssetInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
